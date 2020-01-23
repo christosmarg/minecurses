0a6104389bb7a265c00912f4798df2fb437f31b3
Christos Margiolis <christos@christosmarg.xyz>
2020-01-23 23:41:29 +0200

    removed an M from settings.h

--- include/settings.h
+++ include/settings.h
@@ -5,7 +5,7 @@
 
 #define COLS_CHAR_LENGTH strlen("Columns (Min = 5, Max = XXX): ") + 1
 #define ROWS_CHAR_LENGTH strlen("Rows (Min = 5, Max = YYY): ") + 1
-#define NMINES_CHAR_LENGTH strlen("Mines (Max = MMMΜ): ") + 1
+#define NMINES_CHAR_LENGTH strlen("Mines (Max = MMM): ") + 1
 
 int set_cols(struct _win_st*, int);
 int set_rows(struct _win_st*, int);
