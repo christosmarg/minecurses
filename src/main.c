8f43f0254b6f1a7b3a74378ea5303d4db07e8d27
Christos Margiolis <christos@christosmarg.xyz>
2020-05-18 23:35:02 +0300

    added ncurses check

--- src/main.c
+++ src/main.c
@@ -9,6 +9,10 @@
 int
 main(int argc, char **argv)
 {	
+#ifndef NCURSES_VERSION
+	fprintf(stderr, "ncurses is needed in order to run this program.\n");
+	return EXIT_FAILURE;
+#endif /* NCURSES_VERSION */
 	init_curses();
 	Board b;
 	reset(&b);
@@ -16,25 +20,24 @@ main(int argc, char **argv)
 	init_game(&b);
 
 	pthread_t audiothread;
-	long threadid = 1;
-	pthread_create(&audiothread, NULL, play_audio, (void *)threadid);
+	long tid = 1;
+	pthread_create(&audiothread, NULL, play_audio, (void *)tid);
 	play(&b);
 	
 	pthread_cancel(audiothread);
 	clear_board(&b);
 	delwin(b.gw);
 	endwin();
-
-	return 0;
+	return EXIT_SUCCESS;
 }
 
 void
 reset(Board *b)
 {
 	echo();
-	b->cols = set_cols();
-	b->rows = set_rows();
-	b->nmines = set_nmines(b->rows * b->cols);
+	b->cols		= set_cols();
+	b->rows		= set_rows();
+	b->nmines	= set_nmines(b->rows * b->cols);
 	b->ndefused = b->x = b->y = 0;
 	b->gameover = FALSE;
 	noecho();
